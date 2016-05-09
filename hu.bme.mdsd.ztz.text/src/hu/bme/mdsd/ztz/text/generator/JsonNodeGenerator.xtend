c4e99cffb3e65f3ec83df75550d7b447bda68d47
ZsoltKovari <kovari.zsolt91@gmail.com>
2016-05-09 20:02:58 +0200

    Serialize action properties

--- hu.bme.mdsd.ztz.text/src/hu/bme/mdsd/ztz/text/generator/JsonNodeGenerator.xtend
+++ hu.bme.mdsd.ztz.text/src/hu/bme/mdsd/ztz/text/generator/JsonNodeGenerator.xtend
@@ -4,6 +4,9 @@ import com.fasterxml.jackson.databind.node.JsonNodeFactory
 import com.fasterxml.jackson.databind.node.ObjectNode
 import hu.bme.mdsd.ztz.model.behaviour.Action
 import hu.bme.mdsd.ztz.model.behaviour.DynamicRobot
+import org.eclipse.emf.common.util.EList
+import hu.bme.mdsd.ztz.model.drone.StringValue
+import hu.bme.mdsd.ztz.model.drone.MeasureValue
 
 class JsonNodeGenerator {
 
@@ -17,7 +20,25 @@ class JsonNodeGenerator {
 		val nestedNode = factory.objectNode
 		nestedNode.put("Action", action.name)
 		nestedNode.put("Robot", robot.name)
+		
+		newPropertiesNode(nestedNode, action.properties)
+		
 		node.set("Action", nestedNode)
+	}
+
+	def newPropertiesNode(ObjectNode containerNode, EList<hu.bme.mdsd.ztz.model.drone.Property> properties) {
+		val propertyKey = "Properties"
+		if (properties != null && !properties.empty) {
+			val ObjectNode propertiesNode = factory.objectNode
+			for (hu.bme.mdsd.ztz.model.drone.Property property : properties) {
+				if (property.value instanceof StringValue) {
+					propertiesNode.put(property.key.name, (property.value as StringValue).value)
+				} else if (property.value instanceof MeasureValue) {
+					propertiesNode.put(property.key.name, (property.value as MeasureValue).value)
+				}
+			}
+			containerNode.set(propertyKey, propertiesNode)
+		}
 	}
 
 }
--- hu.bme.mdsd.ztz.text/xtend-gen/hu/bme/mdsd/ztz/text/generator/JsonNodeGenerator.java
+++ hu.bme.mdsd.ztz.text/xtend-gen/hu/bme/mdsd/ztz/text/generator/JsonNodeGenerator.java
@@ -3,8 +3,15 @@ package hu.bme.mdsd.ztz.text.generator;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.google.common.base.Objects;
 import hu.bme.mdsd.ztz.model.behaviour.Action;
 import hu.bme.mdsd.ztz.model.behaviour.DynamicRobot;
+import hu.bme.mdsd.ztz.model.drone.MeasureValue;
+import hu.bme.mdsd.ztz.model.drone.Property;
+import hu.bme.mdsd.ztz.model.drone.PropertyKey;
+import hu.bme.mdsd.ztz.model.drone.PropertyValue;
+import hu.bme.mdsd.ztz.model.drone.StringValue;
+import org.eclipse.emf.common.util.EList;
 
 @SuppressWarnings("all")
 public class JsonNodeGenerator {
@@ -23,7 +30,55 @@ public class JsonNodeGenerator {
       nestedNode.put("Action", _name);
       String _name_1 = robot.getName();
       nestedNode.put("Robot", _name_1);
+      EList<Property> _properties = action.getProperties();
+      this.newPropertiesNode(nestedNode, _properties);
       _xblockexpression = node.set("Action", nestedNode);
+    }
+    return _xblockexpression;
+  }
+  
+  public JsonNode newPropertiesNode(final ObjectNode containerNode, final EList<Property> properties) {
+    JsonNode _xblockexpression = null;
+    {
+      final String propertyKey = "Properties";
+      JsonNode _xifexpression = null;
+      boolean _and = false;
+      boolean _notEquals = (!Objects.equal(properties, null));
+      if (!_notEquals) {
+        _and = false;
+      } else {
+        boolean _isEmpty = properties.isEmpty();
+        boolean _not = (!_isEmpty);
+        _and = _not;
+      }
+      if (_and) {
+        JsonNode _xblockexpression_1 = null;
+        {
+          final ObjectNode propertiesNode = this.factory.objectNode();
+          for (final Property property : properties) {
+            PropertyValue _value = property.getValue();
+            if ((_value instanceof StringValue)) {
+              PropertyKey _key = property.getKey();
+              String _name = _key.getName();
+              PropertyValue _value_1 = property.getValue();
+              String _value_2 = ((StringValue) _value_1).getValue();
+              propertiesNode.put(_name, _value_2);
+            } else {
+              PropertyValue _value_3 = property.getValue();
+              if ((_value_3 instanceof MeasureValue)) {
+                PropertyKey _key_1 = property.getKey();
+                String _name_1 = _key_1.getName();
+                PropertyValue _value_4 = property.getValue();
+                float _value_5 = ((MeasureValue) _value_4).getValue();
+                propertiesNode.put(_name_1, _value_5);
+              }
+            }
+          }
+          _xblockexpression_1 = containerNode.set(propertyKey, propertiesNode);
+        }
+        _xifexpression = _xblockexpression_1;
+      }
+      _xblockexpression = _xifexpression;
     }
     return _xblockexpression;
   }
